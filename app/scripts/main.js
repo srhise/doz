876bc8e8f726f45aff5b97bede5b2bfc85bb7eec
srhise <srhise@gmail.com>
2014-03-26 13:04:18 -0400

    New page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,30 @@
 $('#navigation-link').on('click', function() {
-	console.log('clicked');
-	$('#mainnavrow').slideToggle(200, function(){
-		if ($(this).is(":hidden")) {
-			$('#navigation-link i').removeClass('fa-minus-circle');
-			$('#navigation-link i').addClass('fa-plus-circle');
-		} else {
-			$('#navigation-link i').removeClass('fa-plus-circle');
-			$('#navigation-link i').addClass('fa-minus-circle');
-		}
-	});
+    console.log('clicked');
+    $('#mainnavrow').slideToggle(200, function() {
+        if ($(this).is(":hidden")) {
+            $('#navigation-link i').removeClass('fa-minus-circle');
+            $('#navigation-link i').addClass('fa-plus-circle');
+        } else {
+            $('#navigation-link i').removeClass('fa-plus-circle');
+            $('#navigation-link i').addClass('fa-minus-circle');
+        }
+    });
 });
 
 $('#main-nav-container a').tooltip({
-	"html" : false
+    "html": false
 });
 
 if (!Modernizr.svg) {
-  $("#logo img").attr("src", "images/logo-530x235.png");
+    $("#logo img").attr("src", "images/logo-530x235.png");
 }
+
+$(document).ready(function() {
+    $('.services-popover').popover({
+        html: true,
+        content: function() {
+            return $('#services_dropdown_content').html();
+        }
+    });
+  
+});
